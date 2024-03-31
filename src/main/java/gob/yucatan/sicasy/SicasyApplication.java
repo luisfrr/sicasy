c3255bb792b67863a3eea56f38489c6289e26db1
Luis Fernando Rogel Rosado <lrogel@marista.edu.mx>
2024-03-30 19:29:54 -0600

    Inicio del proyecto

---
+++ src/main/java/gob/yucatan/sicasy/SicasyApplication.java
@@ -0,0 +1,13 @@
+package gob.yucatan.sicasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SicasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gob/yucatan/sicasy/SicasyApplicationTests.java
@@ -0,0 +1,13 @@
+package gob.yucatan.sicasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
