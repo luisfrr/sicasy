11b2961d5d4cdd58d5daf62bdb60c784e101ef31
Luis Fernando Rogel Rosado <lrogel@marista.edu.mx>
2024-04-11 15:43:20 -0600

    Se agrega método genérico de mensajes

---
+++ src/main/java/gob/yucatan/sicasy/views/beans/Messages.java
@@ -0,0 +1,36 @@
+package gob.yucatan.sicasy.views.beans;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+
+public class Messages {
+
+    public static void addMessage(FacesMessage.Severity severity, String summary, String detail) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, summary, detail));
+    }
+
+    public static void addInfo(String summary, String detail) {
+        addMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+    }
+
+    public static void addInfo(String detail) {
+        addMessage(FacesMessage.SEVERITY_INFO, "¡Todo listo!", detail);
+    }
+
+    public static void addError(String summary, String detail) {
+        addMessage(FacesMessage.SEVERITY_ERROR, summary, detail);
+    }
+
+    public static void addError(String detail) {
+        addMessage(FacesMessage.SEVERITY_ERROR, "Error", detail);
+    }
+
+    public static void addWarn(String summary, String detail) {
+        addMessage(FacesMessage.SEVERITY_WARN, summary, detail);
+    }
+
+    public static void addWarn(String detail) {
+        addMessage(FacesMessage.SEVERITY_WARN, "Advertencia", detail);
+    }
+
+}
