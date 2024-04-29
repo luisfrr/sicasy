5d9b6e13c099138356eb5b185f70d68164ee7889
Isra Fuentes <jorge.israfuentes@gmail.com>
2024-04-28 22:40:40 -0600

    FileDownloadBean metodo descarga archivo por filepath

--- src/main/java/gob/yucatan/sicasy/utils/imports/excel/FileDownloadBean.java
+++ src/main/java/gob/yucatan/sicasy/utils/imports/excel/FileDownloadBean.java
@@ -1,14 +1,18 @@
 package gob.yucatan.sicasy.utils.imports.excel;
 
 import gob.yucatan.sicasy.utils.export.ExportFile;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.primefaces.model.DefaultStreamedContent;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
-import java.io.ByteArrayInputStream;
-import java.io.Serializable;
+import java.io.*;
 
+@Slf4j
 @Component
 @RequestScope
 @Getter
@@ -23,6 +27,31 @@ public class FileDownloadBean implements Serializable {
                 .name(exportFile.isUseOriginalFileName() ? exportFile.getFileName() : exportFile.getUniqueFileName())
                 .contentType(exportFile.getExportFileType().getContentType())
                 .build();
+    }
+
+    public DefaultStreamedContent downloadFileByPath(String filePath) {
+        log.info("downloadFile: {}", filePath);
+        File fi = new File(filePath);
+        try {
+            if (!fi.exists()) {
+                FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                        "Error", "No se encontró el archivo.");
+                FacesContext.getCurrentInstance().addMessage(null, msg);
+            }
+            InputStream input = new FileInputStream(fi);
+            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+
+            return DefaultStreamedContent.builder()
+                    .name(fi.getName())
+                    .contentType(externalContext.getMimeType(fi.getName()))
+                    .stream( () -> input )
+                    .build();
+        }catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
+
+
     }
 
 }
