791dbde1aa9a3ded9b58925eff159a8eb6c17d22
Luis Fernando Rogel Rosado <lrogel@marista.edu.mx>
2024-05-28 01:25:29 -0600

    Optimize imports

--- src/main/java/gob/yucatan/sicasy/repository/iface/IPolizaRepository.java
+++ src/main/java/gob/yucatan/sicasy/repository/iface/IPolizaRepository.java
@@ -5,11 +5,9 @@ import gob.yucatan.sicasy.business.enums.EstatusRegistro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 public interface IPolizaRepository extends JpaRepository<Poliza, Long>, JpaSpecificationExecutor<Poliza> {
 
