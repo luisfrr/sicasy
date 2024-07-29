d4864371fb7d3b45bc140359323b3285c242b8b6
Luis Fer <luisfrr27@gmail.com>
2024-07-29 01:40:40 -0600

    Validator JS

---
+++ src/main/webapp/resources/js/validator.js
@@ -0,0 +1,17 @@
+let Validator = {
+
+    validateInputText: function (input) {
+        let regex = /^[a-zA-Z0-9]*$/;
+        if (!regex.test(input.value)) {
+            input.value = input.value.replace(/[^a-zA-Z0-9]/g, '');
+        }
+    },
+
+    validateInputTextUsername: function (input) {
+        let regex = /^[a-zA-Z0-9._]*$/;
+        if (!regex.test(input.value)) {
+            input.value = input.value.replace(/[^a-zA-Z0-9._]/g, '');
+        }
+    }
+
+}
